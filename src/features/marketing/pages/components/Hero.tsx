086902d9128f523f0c9eb2272b14de14e3095e6c
JashawnRogers <rogersjashawn20@gmail.com>
2026-04-16 09:17:32 -0700

    Reduced top padding and margin in hero

--- src/features/marketing/pages/components/Hero.tsx
+++ src/features/marketing/pages/components/Hero.tsx
@@ -1,6 +1,6 @@
 export default function Hero() {
   return (
-    <section className="py-12 my-24 md:py-20">
+    <section className="py-16 my-16">
       <div className="mx-auto max-w-5xl space-y-8 px-6 md:space-y-16">
         <div className="relative z-10 mx-auto max-w-xl space-y-6 text-center">
           <h2 className="text-4xl font-medium lg:text-6xl italic">
